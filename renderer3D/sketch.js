ed6c8226af14c422b7ee1ba63da224a7c50dd8cc
TheGrinchyYak <jacopo.greenslade@gmail.com>
2021-12-12 21:34:27 -0500

    Added more to the demo

--- renderer3D/sketch.js
+++ renderer3D/sketch.js
@@ -42,18 +42,20 @@ function initRenderer() {
     [0, 0, q, -zNearPlane * q],
     [0, 0, 1, 0],
   ]);
-  triangle = [
-    // x1   y1   z1    x2   y2   z2    x3   y3   z3
-    math.matrix([0.0, -0.5, 0.0, 1.0]),
-    math.matrix([-0.5, 0.5, 0.0, 1.0]),
-    math.matrix([0.5, 0.5, 0.0, 1.0]),
-  ];
 
   triangle1 = new Triangle(
     math.matrix([0.0, -0.5, 0.0, 1.0]),
     math.matrix([-0.5, 0.5, 0.0, 1.0]),
     math.matrix([0.5, 0.5, 0.0, 1.0]),
     new Transform(math.matrix([0.0, 0.0, -4.0]))
+  );
+
+  // Tri (haha) giving it a different pivot point
+  triangle2 = new Triangle(
+    math.matrix([3.0, -0.5, 0.0, 1.0]),
+    math.matrix([2.5, 0.5, 0.0, 1.0]),
+    math.matrix([3.5, 0.5, 0.0, 1.0]),
+    new Transform(math.matrix([0.0, 0.0, -5.0]))
   );
 
   getRotateZ = (time) =>
@@ -115,38 +117,27 @@ function setup() {
 function processTriangle(t) {
 
   const modifiedTriangle = new Triangle();
-  // modifiedTriangle.p1 = math.multiply(t.p1, getRotateX(millis()));
-  // modifiedTriangle.p2 = math.multiply(t.p2, getRotateX(millis()));
-  // modifiedTriangle.p3 = math.multiply(t.p3, getRotateX(millis()));
-
-  // modifiedTriangle.p1 = math.multiply(modifiedTriangle.p1, getRotateZ(2 * millis()));
-  // modifiedTriangle.p2 = math.multiply(modifiedTriangle.p2, getRotateZ(2 * millis()));
-  // modifiedTriangle.p3 = math.multiply(modifiedTriangle.p3, getRotateZ(2 * millis()));
 
   const translation =  math.identity(4, 4).subset(math.index(3,[0,1,2]), t.transform.location);
 
   const xAngle = t.transform.rotation.subset(math.index(0));
   const yAngle = t.transform.rotation.subset(math.index(1));
   const zAngle = t.transform.rotation.subset(math.index(2));
-
-  console.log(xAngle, yAngle, zAngle);
-
   const rotationX =  math.matrix([
     [1, 0, 0, 0], 
     [0, cos(xAngle), sin(xAngle), 0], 
     [0, -sin(xAngle), cos(xAngle), 0], 
     [0, 0, 0, 1]]);
-    const rotationY =  math.matrix([
-      [cos(yAngle), 0, -sin(yAngle), 0], 
-      [0, 1, 0, 0], 
-      [sin(yAngle), 0, cos(yAngle), 0], 
-      [0, 0, 0, 1]]);
-    const rotationZ =  math.matrix([
-      [cos(zAngle), sin(zAngle), 0, 0], 
-      [-sin(zAngle), cos(zAngle),0, 0], 
-      [0, 0, 1, 0], 
-      [0, 0, 0, 1]]);
-
+  const rotationY =  math.matrix([
+    [cos(yAngle), 0, -sin(yAngle), 0], 
+    [0, 1, 0, 0], 
+    [sin(yAngle), 0, cos(yAngle), 0], 
+    [0, 0, 0, 1]]);
+  const rotationZ =  math.matrix([
+    [cos(zAngle), sin(zAngle), 0, 0], 
+    [-sin(zAngle), cos(zAngle),0, 0], 
+    [0, 0, 1, 0], 
+    [0, 0, 0, 1]]);
   const rotation = math.multiply(rotationZ, rotationY, rotationX);
 
   const scale = math.matrix([
@@ -154,19 +145,11 @@ function processTriangle(t) {
     [0, t.transform.scale.subset(math.index(1)), 0, 0], 
     [0, 0, t.transform.scale.subset(math.index(2)), 0], 
     [0, 0, 0, 1]]);
-
-  modifiedTriangle.p1 = math.multiply(t.p1, scale, rotation, translation);
-  modifiedTriangle.p2 = math.multiply(t.p2, scale, rotation, translation);
-  modifiedTriangle.p3 = math.multiply(t.p3, scale, rotation, translation);
-
-  // Move triangle
-  // modifiedTriangle.p1 = math.multiply(modifiedTriangle.p1, math.identity(4, 4).subset(math.index(3,[0,1,2]), t.transform.location));
-  // modifiedTriangle.p2 = math.multiply(modifiedTriangle.p2, math.identity(4, 4).subset(math.index(3,[0,1,2]), t.transform.location));
-  // modifiedTriangle.p3 = math.multiply(modifiedTriangle.p3, math.identity(4, 4).subset(math.index(3,[0,1,2]), t.transform.location));
-
-  modifiedTriangle.p1 = math.multiply(modifiedTriangle.p1, projectionMatrix);
-  modifiedTriangle.p2 = math.multiply(modifiedTriangle.p2, projectionMatrix);
-  modifiedTriangle.p3 = math.multiply(modifiedTriangle.p3, projectionMatrix);
+  
+  // Apply transformations all in 1 go! And projection
+  modifiedTriangle.p1 = math.multiply(t.p1, scale, rotation, translation, projectionMatrix);
+  modifiedTriangle.p2 = math.multiply(t.p2, scale, rotation, translation, projectionMatrix);
+  modifiedTriangle.p3 = math.multiply(t.p3, scale, rotation, translation, projectionMatrix);
 
   // console.log(x, y, z);
   // divide x and y by z... Not sure why this isn't happening automatically...
@@ -177,7 +160,7 @@ function processTriangle(t) {
   modifiedTriangle.p3.subset(math.index(0),modifiedTriangle.p3.subset(math.index(0)) * (1 / modifiedTriangle.p3.subset(math.index(2))));
   modifiedTriangle.p3.subset(math.index(1),modifiedTriangle.p3.subset(math.index(1)) * (1 / modifiedTriangle.p3.subset(math.index(2))));
 
-  // Add 1 to the x and y
+  // Add 1 to the x and y to get it into the positive values
   modifiedTriangle.p1.subset(math.index(0), modifiedTriangle.p1.subset(math.index(0)) + 1.0);
   modifiedTriangle.p1.subset(math.index(1), modifiedTriangle.p1.subset(math.index(1)) + 1.0);
   modifiedTriangle.p2.subset(math.index(0), modifiedTriangle.p2.subset(math.index(0)) + 1.0);
@@ -185,8 +168,7 @@ function processTriangle(t) {
   modifiedTriangle.p3.subset(math.index(0), modifiedTriangle.p3.subset(math.index(0)) + 1.0);
   modifiedTriangle.p3.subset(math.index(1), modifiedTriangle.p3.subset(math.index(1)) + 1.0);
 
-  // console.log(x, y, z);
-
+  // Now move it into screen space
   modifiedTriangle.p1.subset(math.index(0), modifiedTriangle.p1.subset(math.index(0)) * SCREEN_WIDTH * 0.5);
   modifiedTriangle.p1.subset(math.index(1), modifiedTriangle.p1.subset(math.index(1)) * SCREEN_HEIGHT * 0.5);
   modifiedTriangle.p2.subset(math.index(0), modifiedTriangle.p2.subset(math.index(0)) * SCREEN_WIDTH * 0.5);
@@ -220,14 +202,19 @@ function drawTriangle(t) {
 
 function draw() {
   background(150);
+  strokeWeight(5);
+  stroke(color(255, 255, 0));
 
   triangle1.transform.rotation = math.matrix([millis()*0.005, millis()*0.001, 0]);
   triangle1.transform.location = math.matrix([sin(millis()*0.003)*5, 0, -4]);
-
-
   let processedT = processTriangle(triangle1);
-
-  strokeWeight(5);
-  stroke(color(255, 255, 0));
   drawTriangle(processedT);
+
+  stroke(color(255, 0, 255));
+  triangle2.transform.rotation = math.matrix([0, millis()*0.0025, 0]);
+  // triangle1.transform.location = math.matrix([sin(millis()*0.003)*5, 0, -4]);
+  processedT = processTriangle(triangle2);
+
+  drawTriangle(processedT);
+
 }
